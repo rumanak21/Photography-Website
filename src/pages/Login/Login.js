2ec658b48b4813083a028b5b13befdd4df0bab57
Rumana K <rumana2k20@gmail.com>
2022-04-18 20:08:43 +0600

    Google Sign Added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,10 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+
+
 
 const Login = () => {
     const emailRef = useRef('');
@@ -16,6 +18,8 @@ const Login = () => {
         error,
       ] = useSignInWithEmailAndPassword(auth);
 
+      
+
     const handleSubmit = (event) => {
         event.preventDefault()
         const email = emailRef.current.value;
@@ -24,12 +28,29 @@ const Login = () => {
         signInWithEmailAndPassword(email, password);
         navigate('/');
     }
+
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (googleError) {
+        return (
+          <div>
+            <p>Error: {error.message}</p>
+          </div>
+        );
+      }
+      if (googleLoading) {
+        return <p>Loading...</p>;
+      }
+      if (googleUser) {
+        navigate('/');
+      }
+
     return (
         <div className='container w-50 my-5'>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -37,17 +58,20 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                
                 <Button variant="primary" type="submit">
                     Login
                 </Button> <br />
                 <p className='my-2'>New to Genius Car? <Link to="/signup" className='text-primary pe-auto text-decoration-none' >Please Sign Up</Link> </p>
                 
             </Form>
+
+            <span>OR</span> <br />
+            <Button onClick={() => signInWithGoogle()} variant="light" type="submit">
+            <img style={{ 'height': "25px"}} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png" alt="" srcset="" /> Sign In with Google
+                </Button> <br />
         </div>
     );
 };
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -33,19 +33,17 @@ const SignUp = () => {
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formBasicPassword" required>
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                
                 <Button variant="primary" type="submit">
                     Sign Up
                 </Button>
